b57959a2019e7e457bfed1a84c8141dbe6635264
CW Self Paced Student <testselfpaced@gmail.com>
2024-10-02 15:23:17 +0000

    more tests done

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
 'use strict';
 
-const methods = {
-  // ready: function (handler) {
-  //   document.addEventListener('DOMContentLoaded', handler, false);
-  // }
-}
-
 function J$ (selector) {
   return Object.assign(document.querySelectorAll(selector), methods);
 }
@@ -13,6 +7,34 @@ function J$ (selector) {
 J$.ready = function (handler) {
   document.addEventListener('DOMContentLoaded', handler, false)
 }
+
+function loop (coll, iterator) {
+  if (coll.length) {
+    for (var i = 0; i < coll.length; i++) {
+      iterator(coll[i]);
+    }
+  } else {
+    iterator(coll);
+  }
+}
+
+// Functional approach with shared methods
+const methods = {};
+
+methods.addClass = function (className) {
+  loop(this, function (el) {
+    el.classList.add(className);
+  });
+  return this;
+};
+
+methods.removeClass = function (className) {
+  loop(this, function (el) {
+    el.classList.remove(className);
+  });
+  return this;
+};
+
 
 // Allow tests to run on the server (leave at the bottom)
 if (typeof window === 'undefined') {
