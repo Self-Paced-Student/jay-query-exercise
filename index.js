7f0362580f1cdd054f0a6c9a4eec4edc4f7e35f6
CW Self Paced Student <testselfpaced@gmail.com>
2024-10-04 16:01:22 +0000

    all done v2

--- index.js
+++ index.js
@@ -76,47 +76,47 @@ methods.click = function (handler) {
   });
 };
 
-// methods.append = function (content) {
-//   loop(this, function (el) {
-//     el.insertAdjacentHTML('beforeend', content);
-//   });
-//   return this;
-// };
+methods.append = function (content) {
+  loop(this, function (el) {
+    el.insertAdjacentHTML('beforeend', content);
+  });
+  return this;
+};
 
-// methods.text = function (content) {
-//   if (content !== undefined) {
-//     loop(this, function (el) {
-//       el.textContent = content;
-//     });
-//     return this;
-//   } else {
-//     const res = [];
-//     loop(this, function (el) {
-//       res.push(el.textContent);
-//     });
-//     return res.join(' ');
-//   }
-// };
+methods.text = function (content) {
+  if (content !== undefined) {
+    loop(this, function (el) {
+      el.textContent = content;
+    });
+    return this;
+  } else {
+    const res = [];
+    loop(this, function (el) {
+      res.push(el.textContent);
+    });
+    return res.join(' ');
+  }
+};
 
-// methods.data = function (el, key, val) {
+methods.data = function (el, key, val) {
 
-//   if (val) storeData(el, key, val); // Store data
-//   else retrieveData(el, key); // Retrieve data
+  if (val) storeData(el, key, val); // Store data
+  else retrieveData(el, key); // Retrieve data
 
-//   function storeData (el, key, val) {
-//     var id;
-//     if (el.dataUniqueId) id = el.dataUniqueId; // If the element already has a unique ID, use it
-//     else el.dataUniqueId = id = Symbol(); // Otherwise assign a new unique ID to it
-//     if (memo[id] === undefined) memo[id] = {}; // If there's no store yet for the unique ID, let's create one
-//     memo[id][key] = val; // And finally store the data
-//   }
+  function storeData (el, key, val) {
+    var id;
+    if (el.dataUniqueId) id = el.dataUniqueId; // If the element already has a unique ID, use it
+    else el.dataUniqueId = id = Symbol(); // Otherwise assign a new unique ID to it
+    if (memo[id] === undefined) memo[id] = {}; // If there's no store yet for the unique ID, let's create one
+    memo[id][key] = val; // And finally store the data
+  }
 
-//   function retrieveData (el, key) {
-//     var id = el.dataUniqueId;
-//     if (id) return memo[id][key];
-//   }
+  function retrieveData (el, key) {
+    var id = el.dataUniqueId;
+    if (id) return memo[id][key];
+  }
 
-// };
+};
 
 
 // Allow tests to run on the server (leave at the bottom)
